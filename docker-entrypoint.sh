aa7eb68996bf0d901d183c9e99e4546e19ee9f80
Anas Kanhouch <anas.m.kan@gmail.com>
2021-08-31 22:35:10 +0300

    add if build and source folders seprated and fix command args

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ ! -f ./conf.py ]; then
+
+if [ ! -f ./conf.py ] && [ ! -f ./source/conf.py ]; then
     echo "== Sphinx docs is not initialized =="
     echo " - Map a volume to /docs to work on existing documentaiton"
     echo " - Run sphinx-quickstart to create a new set of documentation"
     tail -F /dev/null
 fi
 
-sphinx-autobuild . _build_html -H 0.0.0.0 --poll
+sphinx-autobuild source build/html --host 0.0.0.0 --port 8000 
